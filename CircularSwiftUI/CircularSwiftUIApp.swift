64356c54e169f33cc7a5c5bac1e8b9050bdedd52
Yavuz Ulgar <digitalworksyavuz@gmail.com>
2023-02-21 15:50:40 +0300

    Initial Commit

---
+++ CircularSwiftUI/CircularSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularSwiftUIApp.swift
+//  CircularSwiftUI
+//
+//  Created by Yavuz Ulgar on 21.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct CircularSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
